9722029ecc4620ce5d5839f9a65cf6c141de8499
RovshenAshirov <rovshenashirov1619@gmail.com>
2024-04-11 23:32:35 +0500

    verbose name

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 
 class Blog(models.Model):
-    title = models.CharField(max_length=255)
-    body = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255, verbose_name='başlık')
+    body = models.TextField(verbose_name='İçerik')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='ekleme tarihi')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='guncelleme tarihi')
+
+    class Meta:
+        verbose_name = 'yazı'
+        verbose_name_plural = 'yazılar'
 
     def __str__(self):
         return self.title
