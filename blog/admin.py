073799a177f1b4bbd4201866714931b0e2782372
RovshenAshirov <rovshenashirov1619@gmail.com>
2024-04-11 23:05:22 +0500

    Change site title, site header and index title

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+
+admin.site.site_title = 'Extreme Title'
+admin.site.site_header = 'Extreme Admin Portal'  # It also appears on Login Page
+admin.site.index_title = 'Welcome to Extreme Admin Portal'
